22455814e7fc1529d1f76eb1238c13655a1abf2d
Elvan Rizky Novandi <elvan.rizky@ui.ac.id>
2019-05-11 22:19:16 +0700

    [GREEN] implemented database

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "https://elvanfirstsubmission.azurewebsites.net";
+    $user = "elvanovandi";
+    $pass = "Elvngntnk97";
+    $db = "ElvanFirstSubmission";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
